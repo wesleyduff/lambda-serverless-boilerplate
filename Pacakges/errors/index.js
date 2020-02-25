b1a856e2f7ceebcdd93970aefc8aad78ae1acadd
Wes Duff <wesley.a.duff@gmail.com>
2020-02-24 18:40:57 -0700

    Working api endpoint - lambda calling lambda and returning data

--- Pacakges/errors/index.js
+++ Pacakges/errors/index.js
@@ -2,3 +2,4 @@ export { default as BaseError } from './BaseError';
 export { default as DatabaseError } from './DatabaseError';
 export { default as NotFoundError } from './NotFoundError';
 export { default as LambdaInvokeError } from './LambdaInvokeError';
+export { default as ParseError } from './ParseError';
